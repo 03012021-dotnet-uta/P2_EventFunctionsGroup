2d80ad82bd7e33ec15681b973e507dec82341d5c
Christopher Trimmer <chrisschoolacct@yahoo.com>
2021-04-07 21:22:36 -0400

    fix to context class connection string

--- Repository/Contexts/EventFunctionsContext.cs
+++ Repository/Contexts/EventFunctionsContext.cs
@@ -29,11 +29,10 @@ namespace Repository.Contexts
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
-                    optionsBuilder.UseSqlServer("Server=tcp:eventfunctions.database.windows.net,1433;Initial Catalog=EventFunctionsDB;Persist Security Info=False;User ID=event;Password=functions1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-//                optionsBuilder.UseSqlServer("Server=500-409;Database=EventFunctionsDBTest;Trusted_Connection=True;");
-            }
+//                     optionsBuilder.UseSqlServer("Server=tcp:eventfunctions.database.windows.net,1433;Initial Catalog=EventFunctionsDB;Persist Security Info=False;User ID=event;Password=<>;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+           }
         }
 
         // protected override void OnModelCreating(ModelBuilder modelBuilder)
