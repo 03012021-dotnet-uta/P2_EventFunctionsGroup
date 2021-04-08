b8589f56744b425d85841fadad7a0474f759c13f
unknown <nicholasvvong@gmail.com>
2021-04-08 16:11:30 -0700

    Fixed modelbinding

---
+++ WebApi/wwwroot/index.js
@@ -0,0 +1,32 @@
+const button = document.getElementById("add");
+
+button.addEventListener("click", () => {
+    let user = {
+        firstName: "Nick",
+        lastName: "Test",
+        email: "someemail@gmail.com",
+        password: "Nick"
+    }
+
+    fetch('https://eventfunctionsp2.azurewebsites.net/api/Test/register', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(user),
+    })
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(`Network reponse was not ok (${reponse.status})`);
+        }
+        else
+            return response.json();
+    })
+    .then((jsonReponse) => {
+        console.log(jsonReponse);
+    })
+    .catch(function(err) {
+        console.log("Failed to fetch page: ", err);
+    });
+})
