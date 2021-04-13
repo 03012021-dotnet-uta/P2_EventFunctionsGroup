8c352c62cc6b1a421d85bd552cf87578bb3ea4f4
Christopher Trimmer <chrisschoolacct@yahoo.com>
2021-04-13 12:56:22 -0400

    adding detailEvent testing

--- AngularTest/src/app/events/detailevent/detailevent.component.spec.ts
+++ AngularTest/src/app/events/detailevent/detailevent.component.spec.ts
@@ -1,14 +1,75 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { AlertType, RawDetailEvent } from '@app/_models';
+import { AccountService, AlertService, EventService } from '@app/_services';
+import { Observable, of } from 'rxjs';
 import { DetaileventComponent } from './detailevent.component';
+
 
 describe('DetaileventComponent', () => {
   let component: DetaileventComponent;
   let fixture: ComponentFixture<DetaileventComponent>;
+  let spy: any;
+  let testAlert; 
+
+  class eventStub {
+      getById(id: string): Observable<RawDetailEvent> { 
+        return of ({
+          id: '1',
+          name: 'Team3',
+          date: new Date(),
+          location: 'Kansas',
+          description: 'Test Event Stub',
+          eventType: 'Concert',
+          manager: 'Test Manager',
+          currentAttending: 300,
+          capacity: 300,
+          locationMap: 'test location map',          
+          });
+      }
+
+      onRegisterEvent(userId: string, eventType: string): Observable<string> {
+        return of ('successful');
+      }
+
+
+
+      // registerEvent(userId: string, eventType: string): Observable<string> {
+      //   return of ('successful');
+      // }
+  }
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetaileventComponent ]
+      declarations: [ DetaileventComponent ],
+      providers: [
+        {
+          provide: ActivatedRoute, 
+          
+          useValue: { 
+              queryParams: of ({id: 1})
+           }
+
+        },
+        {
+          provide: EventService,
+          useClass: eventStub
+        },
+        {
+          provide: AccountService,
+          useValue: { AccountService }
+        },
+        {
+          provide: AlertService,
+          useValue: { AlertService }
+        },
+        {
+          provide: Router,
+          useValue: { Router }
+        }
+
+      ]
     })
     .compileComponents();
   }));
@@ -17,9 +78,20 @@ describe('DetaileventComponent', () => {
     fixture = TestBed.createComponent(DetaileventComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should register', () => {
+    testAlert = new AlertService();
+    spy = spyOn(testAlert, 'clear').withArgs('default-alert');
+    expect(component.onRegisterEvent()).toBeTruthy();
+  })
 });
+
+
