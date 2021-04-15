6c131a0129116e62cd4f309ba265900544d295a4
Christopher Trimmer <chrisschoolacct@yahoo.com>
2021-04-15 12:53:03 -0400

    adding account service test file

---
+++ AngularTest/src/app/_services/account.service.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { AccountService } from './account.service';
+
+describe('AccountService Test', () => {
+  let service: AccountService;
+  beforeEach(() => {
+      TestBed.configureTestingModule({
+          providers: [
+              {
+                provide: Router,
+                useValue: { Router }
+              },
+              {
+                  provide: AccountService,
+                  useValue: { AccountService }
+              }
+          ]
+        })
+      service = TestBed.inject(AccountService);
+  });
+
+  it('should be created', () => {
+      expect(service).toBeTruthy();
+  });
+});
